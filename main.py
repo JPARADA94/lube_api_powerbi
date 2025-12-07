99fdeb8a60f8150ed82f778e40c1a0a3ce34833d
JPARADA94 <javier_23_09@hotmail.com>
2025-12-06 23:39:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 TENANT_ID = "d954438c-7f11-4a1a-b7ee-f77d6c48ec0a"
 CLIENT_ID = "70348d88-7eff-4fd4-99ff-dec8cee6afec"
-CLIENT_SECRET = "TU_SECRET_AQUI"
+CLIENT_SECRET = "i_a8Q~uePWggn1~~SMHvCUfEsfWDGlN~27Sxfb_i"
 GROUP_ID = "bd2f044c-eb69-4ebc-b1ed-ebdf71c33b77"
 DATASET_ID = "1e30ac8a-b779-40ed-a18c-883383e44014"
 
@@ -83,3 +83,4 @@ def obtener_datos(req: ComponenteRequest):
     rows = r.json()["results"][0]["tables"][0]["rows"]
 
     return {"componente": req.componente, "filas": rows}
+
